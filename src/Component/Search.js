a602b898f0d321aadd98c94263368b021d4a306c
Rahul-Verma <94426476+rvstarcaptain@users.noreply.github.com>
2022-05-26 11:54:36 +0530

    first commit

---
+++ src/Component/Search.js
@@ -0,0 +1,95 @@
+
+
+import axios from 'axios';
+import React, {useState } from 'react'
+import { Card } from 'react-bootstrap';
+
+
+export default function Search() {
+    const [text, setText] = useState([]);
+    const [movie, setMovie] = useState([]);
+    const [error,setError] =useState(true);
+
+        function getMovieName(e) {
+            e.preventDefault();
+            axios.get(`http://www.omdbapi.com/?s=${text}&apikey=bb7da3c`).then((response) => {
+              
+               
+                console.log(response.data.Search)
+                setMovie(response.data.Search)
+                setText('')
+            
+            }).catch(error =>{
+                setMovie(error,"please write valid movie name")
+            })
+
+    
+        }
+    
+    return (
+        <>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">Navbar</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Link</a>
+                            </li>
+
+
+                        </ul>
+                        <form className="d-flex" role="search" onSubmit={getMovieName}>
+                            <input className="form-control me-2" value={text} onChange={(e) => setText(e.target.value)} type="search" placeholder="Search movies...." aria-label="Search" />
+                            <button className="btn btn-outline-success" type="submit">Search</button>
+                        </form>
+                    </div>
+                </div>
+            </nav>
+
+            <div className='container'>
+                <div className='row'>
+
+                    {
+                        movie.map((value, id) => {
+                            return (
+                                (
+                                    error? <div className='col-3' key={id}>
+                                    <Card style={{ width: '18rem', col: '3' }} key={id} >
+                                        <Card.Img variant="top" src={value.Poster} />
+                                        <Card.Body>
+                                            <Card.Title>{value.Title}</Card.Title>
+                                            <Card.Text>
+                                                {value.Year}
+                                            </Card.Text>
+
+                                        </Card.Body>
+                                    </Card>
+
+                                </div>:"please write valid movie name"
+                                )
+                                   
+                                
+
+
+
+
+
+
+                            )
+                        })
+                    }
+
+                </div>
+
+            </div>
+
+        </>
+    )
+}
